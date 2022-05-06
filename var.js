242a3f2389c6e2ca3335e230ae3c1ec7de2b4b27
Thang cg <duongsag.hp11@gmail.com>
2022-05-06 09:16:46 +0700

    update

---
+++ var.js
@@ -0,0 +1,32 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+document.write('<br/>')
+document.write('<br/>')
+document.write('<br/>')
+document.write('<br/>')
+let width=20
+let height=10
+let area =width*height
+document.write('Area = '+ area)
+document.write('<br/>')
+document.write('<br/>')
+document.write('<br/>')
+document.write('<br/>')
+let a=prompt('Nhập a')
+    a=parseInt(a)
+let c=prompt('Nhập c')
+    c=parseInt(c)
+if(a%c===0)
+    alert( 'a là bội số của b')
+else
+    alert('Khong phai ')
